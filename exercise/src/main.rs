ce3b7956a51003d29de867106574e0ff2f48b43a
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-12-25 17:40:19 +0900

    feat: write some number sequence

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, 🌎!");
+
+    let mut x: i32 = 6;
+    print!("{x}");
+    while x != 1 {
+        if x % 2 == 0 {
+            x /= 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+    println!();
 }
